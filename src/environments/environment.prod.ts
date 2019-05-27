e9b6df3a6f39fe4e983f32e98fb89e9f20aadccc
Nelson <37249915+nelsob44@users.noreply.github.com>
2019-05-27 18:34:36 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  googleMapsAPIKey: 'AIzaSyDdCte0L4b4XoEZoR-YHwC8PqTCPDr9rOY',
-  firebaseAPIKey: 'AIzaSyB4eWigOkIw7YnXDDvXikp0R2JrWsnVygw'
+  googleMapsAPIKey: '',
+  firebaseAPIKey: ''
 };
